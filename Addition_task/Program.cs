b48fa3cb67df6d630b41a4f2fd73add583f5296d
Loarlam <messiah02@mail.ru>
2020-03-25 19:12:36 +0300

    Add project files.

---
+++ Addition_task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Addition_task
+{
+    class Program
+    {
+        delegate double MyDelegateWithInputParameter(int myFirstParameter, int mySecondParameter, int myThirdParameter);
+
+        static void Main(string[] args)
+        {
+            MyDelegateWithInputParameter myDWithInputParameter;
+            int firstParameter = 1, secondParameter = 2, thirdParameter = 3;
+
+            myDWithInputParameter = (myFirstParameter, mySecondParameter, myThirdParameter) => (myFirstParameter + mySecondParameter + myThirdParameter) / 3;
+
+            Console.WriteLine($"Среднее арифметическое равно ({firstParameter}+{secondParameter}+{thirdParameter})/3 = {myDWithInputParameter(firstParameter, secondParameter, thirdParameter)}");
+            Console.ReadKey();
+        }
+    }
+}
